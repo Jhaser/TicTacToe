cdab286b616e48da73f7b21cffaaeb64ed7e224e
Jhaser_journey <74832842+Jhaser@users.noreply.github.com>
2021-09-10 22:23:58 +0800

    Add files via upload

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        //instantiate and initiate List property
+        private static List<char> TTT = new List<char>() 
+            {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        private static int flag = 0; //valid, continue
+        private static int player = 1;
+        private static int index;
+        static void Main()
+        {
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("TicTacToe Game: 2 Player X O");
+                Console.WriteLine("Instructions: Input a number to mark X or O on game board.");
+                    Console.WriteLine("\n");
+                TicTacToe.WhoseTurn(player);
+                    Console.WriteLine("\n");
+                TicTacToe.DisplayBoard(TTT);
+                    Console.WriteLine("\n");
+
+                try {
+                    Console.Write("NUMBER: ");
+                    index = Convert.ToInt32(Console.ReadLine()); }
+                catch {
+                    Console.WriteLine("ERROR: Input is invalid. Please enter a NUMBER. "); }
+
+                InputArr(TTT, index);
+            } while (flag != 1 && flag != 2 && flag != 3);
+            Console.Clear();
+            TicTacToe.OutputState(TTT);
+        }
+
+        private static void InputArr(List<char> list, int index)
+        {
+            if (list[index] != 'X' && list[index] != 'O')
+            {  //check whether input index is alrd marked
+                if (player % 2 == 1)
+                { list[index] = 'X';  //player1
+                    player++; }
+                else
+                { list[index] = 'O';
+                    player++; } } //player2
+            else
+            { TicTacToe.Print(7); }
+            flag = TicTacToe.OutputState(list);
+        }
+    }
+}
+
+
+
+
+
+//For testing
+// char[,] stateWin = 
+// {
+//     { 'O', '.', 'X' },
+//     { '.', 'O', 'X' },
+//     { '.', '.', 'X'}
+// };
+// Console.WriteLine("Output of stateWin: ");
+// TicTacToe.OutputState(stateWin);
+
+// char[,] stateDraw =
+//{
+//          { 'O', 'X', 'O'},
+//          { 'X', 'O', 'X'},
+//          { 'X', 'O', 'X'}
+//      };
+// Console.WriteLine("Output of stateDraw: ");
+// TicTacToe.OutputState(stateDraw);
+
+// char[,] stateXturn =
+//{
+//     { 'O', '.', 'X' },
+//     { '.', 'O', 'X' },
+//     { '.', '.', '.'}
+// };
+// Console.WriteLine("Output of stateXturn: ");
+// TicTacToe.OutputState(stateXturn);
+
+// char[,] stateInvalid =
+// {
+//          { 'O', '.', 'X'},
+//          { '.', 'O', 'X'},
+//          { '.', '.', 'O'}
+//      };
+// Console.WriteLine("Output of stateInvalid: ");
+// TicTacToe.OutputState(stateInvalid);
---
+++ TicTacToe/TicTacToe.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class TicTacToe
+    {
+        public static int OutputState(List<char> TTT)
+        {
+            int turns = 0;
+            foreach (char ch in TTT) {              //count every X and O
+                if (ch == 'X' && ch == 'O') { turns++; } }
+
+            if (CheckWin(TTT) == 1 && WhoseTurn(turns) == 1) { Print(2); return 2; }       //X wins
+            else if (CheckWin(TTT) == 1 && WhoseTurn(turns) == 2) { Print(3); return 2; }  //O wins
+            else if (CheckDraw(TTT) == 1) { Print(4); return 3; } //draw
+            return 0;
+        }
+
+        //Methods
+        private static int CheckDraw(List<char> list)
+        {
+            if(list.Count ==9)
+            { return 1; }
+            return 0;
+        }
+
+        private static int CheckWin(List<char> T)   //1 means win, 0 means never win
+        {
+            for (int i = 1; i < 4; i++)
+            {
+                if ((T[i] == T[i + 3]) && (T[i + 3] == T[i + 6]))
+                { //vertical win
+                    return 1;
+                }
+            }
+            if ((T[1] == T[2] && T[2] == T[3]) ||
+                (T[4] == T[5] && T[5] == T[6]) ||
+                (T[7] == T[8] && T[8] == T[9]))
+            {      //horizontal win
+                return 1;
+            }
+            if ((T[1] == T[5] && T[5] == T[9]) ||  //diagonal win
+                (T[3] == T[5] && T[5] == T[7]))
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        public static int WhoseTurn(int num)
+        {
+            if (num % 2 == 1) { Print(5); return 1; } //player 1=X
+            Print(6); return 2; //player 2=O
+        }
+
+        public static void Print(int n)
+        {
+            switch(n)
+            {
+                case 1: Console.WriteLine("Wait, what?"); break;
+                case 2: Console.WriteLine("X wins!"); break;
+                case 3: Console.WriteLine("O wins!"); break;
+                case 4: Console.WriteLine("Its a draw."); break;
+                case 5: Console.WriteLine("X Turn."); break;
+                case 6: Console.WriteLine("O Turn."); break;
+                case 7: Console.WriteLine("Position is already taken.");
+                        Console.WriteLine("Please wait 2 seconds board is loading again.");
+                        Thread.Sleep(2000);
+                        break;
+            }
+        }
+        public static void DisplayBoard(List<char> TTT)
+        {
+            Console.WriteLine("    -- -- -- -- -- -- -- -- -- ");
+            Console.WriteLine("     {0}   ||   {1}   ||   {2}", TTT[1], TTT[2], TTT[3]);
+            Console.WriteLine("    --------------------------");
+            Console.WriteLine("     {0}   ||   {1}   ||   {2}", TTT[4], TTT[5], TTT[6]);
+            Console.WriteLine("    ---------------------------");
+            Console.WriteLine("     {0}   ||   {1}   ||   {2}", TTT[7], TTT[8], TTT[9]);
+            Console.WriteLine("    -- -- -- -- -- -- -- -- --");
+        }
+    }
+}
+
